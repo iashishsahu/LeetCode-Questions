a40191285b2a01c60c330d53cabfbb088bf79343
iashishsahu <73061969+iashishsahu@users.noreply.github.com>
2022-04-18 11:46:14 +0530

    Time: 22 ms (62.89%), Space: 24.2 MB (60.91%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -12,16 +12,19 @@
 class Solution {
 public: 
     
-    void inorder(TreeNode* root, vector<int>& ans) {
+    void inorder(TreeNode* root, int &k ,int &ans) {
         if(root == NULL)
             return ;
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
+        inorder(root->left,k,ans);
+        k--;
+        if(k==0){
+        ans=root->val;
+        }
+        inorder(root->right ,k,ans);
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> v;
-        inorder(root,v);
-        return v.at(k-1);
+        int ans=0;
+        inorder(root,k,ans);
+        return ans;
     }
 };

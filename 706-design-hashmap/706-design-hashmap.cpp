0129290d4125fec54cea4693ff89fd314216a57e
iashishsahu <73061969+iashishsahu@users.noreply.github.com>
2022-04-22 20:32:04 +0530

    Time: 177 ms (59.72%), Space: 52 MB (88.67%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,23 +1,52 @@
 class MyHashMap {
 public:
-    vector<int> v;
-    int siz;
+    vector<list<pair<int,int>>>mp;
+    int size=10;
     MyHashMap() {
-        siz=1e6+1;
-        v.resize(siz);
-        fill(v.begin(), v.end(),-1);
+       mp.resize(size); 
     }
-    
+    int hash(int key)
+    {
+        return key%size;
+    }
     void put(int key, int value) {
-        v[key]=value;
+       int i=hash(key);
+        for(auto it=mp[i].begin();it!=mp[i].end();it++)
+        {
+            if(it->first==key)
+            {
+                it->second=value;
+                return;
+            }
+        }
+        mp[i].push_back({key,value});
     }
     
     int get(int key) {
-        return v[key];
+       
+        int i=hash(key);
+        for(auto it=mp[i].begin();it!=mp[i].end();it++)
+        {
+            if(it->first==key)
+            {
+                return it->second;
+            
+            }
+        }
+        return -1;
     }
     
     void remove(int key) {
-        v[key]=-1;
+      
+        int i=hash(key);
+        for(auto it=mp[i].begin();it!=mp[i].end();it++)
+        {
+            if(it->first==key)
+            {
+               mp[i].erase(it);
+                return;
+            }
+        }
     }
 };
 

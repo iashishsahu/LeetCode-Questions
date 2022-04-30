a417dcff5e17541bf0354c8b34a55dd42ce7012d
iashishsahu <73061969+iashishsahu@users.noreply.github.com>
2022-04-30 09:26:55 +0530

    Time: 3 ms (67.46%), Space: 9.8 MB (5.65%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
-    void solvedfs(string s, string d, map<string,vector<pair<string,double>>>& m, set<string> &vis, double &anss, double temp){
-        if(vis.find(s)!=vis.end()){
-            return ;
-        }
-        else{
-            vis.insert(s);
-            if(s==d){
-                anss=temp;
-                return ;
-            }
-            else{
-                for(auto it : m[s]){
-                    solvedfs(it.first, d, m, vis, anss, temp*it.second);
-                }
+    double dfs(string a,string&b,unordered_map<string,unordered_map<string,double>>graph,unordered_set<string>&visited)
+    {
+        
+    if(graph[a].find(b)!=graph[a].end())
+    {
+       return graph[a][b]; 
+    }
+for(auto node :graph[a])
+    {
+        if(visited.find(node.first)==visited.end())
+        {
+            visited.insert(node.first);
+            double val=dfs(node.first,b,graph,visited);
+            if(val)
+            {
+                graph[a][b]=node.second*val;
+                return graph[a][b];
             }
         }
     }
-    vector<double> calcEquation(vector<vector<string>>& e, vector<double>& v, vector<vector<string>>& q) {
-        vector<double> ans;
-        map<string,vector<pair<string, double>>> mp;
-        for(int i=0; i<e.size(); i++){
-            mp[e[i][0]].push_back({e[i][1], v[i]});
-            mp[e[i][1]].push_back({e[i][0], 1/v[i]});
-        }
-        for(int i=0; i<q.size(); i++){
-            string s = q[i][0];
-            string d = q[i][1];
-            double anss = -1.0;
-            set<string> vis;
-            if(mp.find(s)!=mp.end()){
-                solvedfs(s, d, mp, vis, anss, 1.0);
-            }
-            ans.push_back(anss);
-        }
-        return ans;  
+        return 0;
+    }
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+       vector<double>ans;
+       unordered_map<string,unordered_map<string,double>>graph;
+       for(int i=0;i<equations.size();i++)
+       {
+           graph[equations[i][0]][equations[i][1]]=values[i];
+           graph[equations[i][1]][equations[i][0]]=(double) 1/(values[i]);
+       }
+       for(int i=0;i<queries.size();i++)
+       {
+           unordered_set<string>visited;
+           double val=dfs(queries[i][0],queries[i][1],graph,visited);
+           if(!val)
+           {
+               ans.push_back(-1.0);
+           }
+           else
+           {
+               ans.push_back(val);
+           }
+       }
+        return ans;
     }
 };

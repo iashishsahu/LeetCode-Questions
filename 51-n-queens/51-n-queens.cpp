867b872fa99f35d9b7f4447b35532ccea51abc2e
iashishsahu <73061969+iashishsahu@users.noreply.github.com>
2022-08-22 01:42:52 +0530

    Time: 6 ms (82.41%), Space: 7.3 MB (52.98%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,57 +1,37 @@
-bool isSafe1(int row, int col, vector < string > board, int n) {
-      // check upper element
-      int duprow = row;
-      int dupcol = col;
-
-      while (row >= 0 && col >= 0) {
-        if (board[row][col] == 'Q')
-          return false;
-        row--;
-        col--;
-      }
-
-      col = dupcol;
-      row = duprow;
-      while (col >= 0) {
-        if (board[row][col] == 'Q')
-          return false;
-        col--;
-      }
-
-      row = duprow;
-      col = dupcol;
-      while (row < n && col >= 0) {
-        if (board[row][col] == 'Q')
-          return false;
-        row++;
-        col--;
-      }
-      return true;
-    }
-void solve(int col, vector < string > & board, vector < vector < string >> & ans, int n) {
+void solve(int col, vector < string > & board, vector < vector < string >> & ans, 
+           vector < int> & leftR,
+    vector < int > & upperD,
+    vector < int > & lowerD, int n) {
       if (col == n) {
-        ans.push_back(board);
-        return;
+          ans.push_back(board);
+          return;
       }
-      for (int row = 0; row < n; row++) {
-        if (isSafe1(row, col, board, n)) {
+    for (int row = 0; row < n; row++) {
+        if (leftR[row] == 0 && lowerD[row + col] == 0 && upperD[n - 1 + col - row] == 0) {
           board[row][col] = 'Q';
-          solve(col + 1, board, ans, n);
+           leftR[row] = 1;
+          lowerD[row + col] = 1;
+          upperD[n - 1 + col - row] = 1;
+            solve(col + 1, board, ans, leftR, upperD, lowerD, n);
           board[row][col] = '.';
+          leftR[row] = 0;
+          lowerD[row + col] = 0;
+          upperD[n - 1 + col - row] = 0;
         }
-      }
     }
+}
 
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector < vector < string >> ans;
-      vector < string > board(n);
-      string s(n, '.');
-      for (int i = 0; i < n; i++) {
-        board[i] = s;
-      }
-      solve(0, board, ans, n);
-      return ans;
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+            for(int i=0 ;i<n; i++){
+                board[i]=s;
+            }
+        vector<int>leftR(n,0),upperD(2*n-1,0),lowerD(2*n-1,0);
+    solve(0,board,ans,leftR,upperD,lowerD,n);
+        return ans;
     }
 };

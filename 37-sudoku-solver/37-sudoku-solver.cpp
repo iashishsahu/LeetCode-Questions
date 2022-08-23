8a4ea9c258e2506c787331163c8caf1a26fa205b
iashishsahu <73061969+iashishsahu@users.noreply.github.com>
2022-08-23 22:33:53 +0530

    Time: 60 ms (30.65%), Space: 6.4 MB (49.83%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -18,9 +18,12 @@ public:
 }
 
 bool solve(vector < vector < char >> & board) {
+    
   for (int i = 0; i < board.size(); i++) {
     for (int j = 0; j < board[0].size(); j++) {
+        
       if (board[i][j] == '.') {
+          
         for (char c = '1'; c <= '9'; c++) {
           if (isValid(board, i, j, c)) {
             board[i][j] = c;

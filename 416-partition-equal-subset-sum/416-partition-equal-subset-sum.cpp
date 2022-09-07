2748476f11d95aad41eac52f1f9ae2ad561194cc
iashishsahu <73061969+iashishsahu@users.noreply.github.com>
2022-09-08 01:27:22 +0530

    Time: 323 ms (54.89%), Space: 12.3 MB (64.84%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool subsetSumToK(int n, int k, vector<int> &arr){
+    vector<bool> prev(k+1,false);
+    
+    prev[0] = true;
+    
+    if(arr[0]<=k)
+        prev[arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        vector<bool> cur(k+1,false);
+        cur[0] = true;
+        for(int target= 1; target<=k; target++){
+            bool notTaken = prev[target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = prev[target-arr[ind]];
+        
+            cur[target]= notTaken||taken;
+        }
+        prev = cur;
+    }
+    
+    return prev[k];
+}
+    
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int totSum=0;
+            for(int i=0 ; i<n; i++){
+                totSum+=nums[i];
+            }
+        if(totSum%2)  return false;
+         int target=totSum/2;
+        
+        
+        return subsetSumToK(n,target,nums);
+    }
+};
